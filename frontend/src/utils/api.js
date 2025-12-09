6263f2d9d180660f01b738fe157a0014eab9a812
THE-ABHIYUV-ERA <abhishekcse112005@gmail.com>
2025-12-09 16:57:13 +0530

    fixed frontend package.json

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
-
+  baseURL: "https://soulbox-backend.onrender.com/api",  // ✅ Your LIVE Backend
 });
 
-// add token automatically
+// Automatically add token
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers["Authorization"] = `Bearer ${token}`;
